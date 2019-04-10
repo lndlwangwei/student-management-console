5367d02d7aa8d6e1e53687595b7a0d254d4f2bc9
lndlwangwei <lndl.wangwei@gmail.com>
2019-04-11 07:50:44 +0800

    丰富学生信息

--- sql/init.sql
+++ sql/init.sql
@@ -3,15 +3,17 @@ create table user
 (
   id        int                       not null
     primary key auto_increment,
-  user_name varchar(30)               null,
-  password  varchar(64)               null,
+  name varchar(30)          not     null,
+  age int not null,
   gender    varchar(10) default 'male' null,
+  card_id  varchar(64)   default     null COMMENT '身份证编号',
+  hometown varchar(512) ,
   create_time datetime not null ,
   update_time datetime not null
 );
 
-create index user_name
-  on user (user_name);
+create index name
+  on user (name);
 
 create table clazz
 (
